865731e3bfac7c6cd5876ecbbe7c2aec2868840a
Maxwolv67 <71527812+Maxwolv67@users.noreply.github.com>
2021-04-23 13:04:12 +0300

    Add files via upload
    Главное меню, вход, регистрация

---
+++ input.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="input.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <a href="index.html"><h2>Назад</h2></a>
+    </div>
+
+    <form action="" method="POST">
+        <p id="lgtxt">Логин</p>
+        <input type="text" id="login">
+
+        <p id="lgps">Пароль</p>
+        <input type="text" id="password">
+
+        <input type="button" id="button" value="Войти">
+    </form>
+
+    <a href="register.php" id="reg">Зарегистрироваться</a>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="register.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <a href="input.php"><h2>Назад</h2></a>
+    </div>
+
+    <form action="" method="POST">
+        <p id="lgtxt">Логин</p>
+        <input type="text" id="login">
+
+        <p id="lgps">Пароль</p>
+        <input type="text" id="password">
+
+        <p id="lgps2">Повторите пароль</p>
+        <input type="text" id="password2">
+
+        <p id="lgml">Email</p>
+        <input type="email" id="mail">
+
+        <input type="button" id="button" value="Регистрация">
+    </form>
+</body>
+</html>
